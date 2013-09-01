44215510c1b22714c8b21daeec8057a86cd65b5f
apaikan <ali.paikan@gmail.com>
2013-09-01 04:57:18 +0200

    simple walking remote control

--- firmware/apps/system/myCub/myCub.cpp
+++ firmware/apps/system/myCub/myCub.cpp
@@ -41,13 +41,15 @@
 #include <ctype.h>
 
 #include <mycub_interface.h>
+#include <mycub_walk.h>
 
- 
+/*
 #include <termios.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/select.h>
 #include <fcntl.h>
+*/
 
 #include <apps/readline.h>
   
@@ -73,141 +75,6 @@ extern "C"
 {
 
 static char mycub_cmd[MAX_CMD_NUM][MAX_CMD_LEN];
-
-void status_serial(long int fd) 
-{
-    /*
-	int status;
-	unsigned int arg;
-	status = ioctl(fd, TIOCMGET, &arg);
-	fprintf(stderr, "[STATUS]: ");
-	if(arg & TIOCM_RTS) fprintf(stderr, "RTS ");
-	if(arg & TIOCM_CTS) fprintf(stderr, "CTS ");
-	if(arg & TIOCM_DSR) fprintf(stderr, "DSR ");
-	if(arg & TIOCM_CAR) fprintf(stderr, "DCD ");
-	if(arg & TIOCM_DTR) fprintf(stderr, "DTR ");
-	if(arg & TIOCM_RNG) fprintf(stderr, "RI ");
-	fprintf(stderr, "\r\n");
-    */
-}
-
-
-
-int init_serial(const char* devicename, int baudrate)
-{
-	int comfd;
-	struct termios oldtio, newtio;       //place for old and new port settings for serial port
-	struct termios oldkey, newkey;       //place tor old and new port settings for keyboard teletype
-
-	comfd = open(devicename, O_RDWR | O_NOCTTY | O_NONBLOCK);
-	if (comfd < 0)
-	{
-		perror(devicename);
-		return comfd;
-	}
-
-    tcgetattr(comfd, &oldtio); // save current port settings 
-	newtio.c_cflag = baudrate | CS8 | CLOCAL | CREAD;
-	newtio.c_iflag = IGNPAR;
-	newtio.c_oflag = 0;
-	newtio.c_lflag = 0;
-	newtio.c_cc[VMIN]=1;
-	newtio.c_cc[VTIME]=0;
-	tcflush(comfd, TCIFLUSH);
-	tcsetattr(comfd,TCSANOW,&newtio);
-    
-    status_serial(comfd);
-    return comfd;
-}
-
-
-inline void write_serial(int outfd, char ch)
-{
-  char buffer = ch;
-  ssize_t nwritten;
-  do
-    {
-
-      nwritten = write(outfd, &buffer, 1);
-      if (nwritten < 0 && errno != EINTR)
-        {
-          break;
-        }
-    }
-  while (nwritten < 1);
-}
-
-inline char read_serial(int infd)
-{
-  char buffer;
-  ssize_t nread;
-
-  do 
-  {
-      nread = read(infd, &buffer, 1);
-      if (nread == 0)
-        {
-          return EOF;
-        }
-
-      else if (nread < 0)
-        {
-          int errcode = errno;
-          if (errcode != EINTR)
-              return EOF;
-        }
-    }
-  while (nread < 1);
-
-  return buffer;
-}
-
-
-void ack_serial(int comfd, char* msg) 
-{
-    for(int i=0; i<(int)strlen(msg); i++)
-        write_serial(comfd, msg[i]);
-}
-
-size_t readline_serial(int comfd, char* buff, size_t maxlen, char* prompt) 
-{
-    char ch;
-    size_t len = 0;
-
-    write_serial(comfd, '\n');
-    if(prompt)
-    {
-        for(int i=0; i<(int)strlen(prompt); i++)
-            write_serial(comfd, prompt[i]);
-    }
-
-    fd_set fds;
-    FD_ZERO(&fds);
-    FD_SET(comfd, &fds);
-
-    do {
-
-        int ret = select(comfd+1, &fds, NULL, NULL, NULL);
-        if(ret == -1)
-        {
-            perror("select");
-            buff[len] = '\0';
-            return len;
-        }        
-        
-        if(FD_ISSET(comfd, &fds)) 
-        {
-            ch = read_serial(comfd);
-            write_serial(comfd, ch);            
-            if(isprint(ch))
-                buff[len++] = ch;
-        }
-
-    } while ((ch != '\n') && (ch != '\r') && (len < maxlen));
-    
-    buff[len] = '\0';
-    return len;
-}
 
 size_t parse_string(char* str)
 {
@@ -250,56 +117,55 @@ int myCub_main(int argc, char *argv[])
     }
 
     printf("-- myCub Intreface --\n");  
-    int comfd = init_serial("/dev/ttyS1", B115200);
-    if(comfd < 0 )
-    {
-        printf("Error in intializing serial port %s.\n", "/dev/ttyS1");
-        return 0;
-    }
 
     MyCubInterface mycub;
     mycub.init();
+    MyCubWalker::init();
 
     char buf[256];
+    should_stop = false;
     while(!should_stop) 
     {
-        ssize_t len = readline_serial(comfd, buf, 256, "[myCub] ");
+        //ssize_t len = readline_serial(comfd, buf, 256, "[myCub] ");
+        printf("[myCub] "); fflush(stdout);
+        ssize_t len = readline(buf, 256, stdin, stdout);
         if(len > 0)
         {
-            buf[len] = '\0';
-            int n = parse_string(buf);            
-            //for (int i=0; i<n; i++)
-            //    printf("command: [%s]\n", mycub_cmd[i]);
-            
+            buf[len-1] = '\0';
+            int n = parse_string(buf);        
+            if(n == 0)
+                continue;
             // executing commands
-            if(strcmp(mycub_cmd[0], "setPose")==0 ) {
+            if(strcmp(mycub_cmd[0], "exit")==0 )
+            {
+                should_stop = true;            
+            }                
+            else if(strcmp(mycub_cmd[0], "setPose")==0 ) {
                 if(n >= 3)
                 {
                     mycub.setPose(atoi(mycub_cmd[1]), atoi(mycub_cmd[2]));
-                    ack_serial(comfd, "[ok]");
+                    printf("[ok]"); fflush(stdout);
                 }
                 else
-                    ack_serial(comfd, "[error]");
+                    printf("[error]"); fflush(stdout);
             }
             else if(strcmp(mycub_cmd[0], "getPose")==0 ) {
                 if(n >= 2)
                 {
                     int pos = mycub.getPose(atoi(mycub_cmd[1]));
-                    sprintf(buf, "%d", pos);
-                    ack_serial(comfd, buf);
+                    printf("%d\n", pos); fflush(stdout);
                 }
                 else
-                    ack_serial(comfd, "[error]");
+                    printf("[error]"); fflush(stdout);
             }
             else if(strcmp(mycub_cmd[0], "getDistance")==0 ) {
                 if(n >= 2)
                 {
                     int dist = mycub.getDistance(atoi(mycub_cmd[1]));
-                    sprintf(buf, "%d", dist);
-                    ack_serial(comfd, buf);
+                    printf("%d\n", dist); fflush(stdout);
                 }
                 else
-                    ack_serial(comfd, "[error]");
+                    printf("[error]"); fflush(stdout);
             }
 
 
@@ -310,10 +176,10 @@ int myCub_main(int argc, char *argv[])
                     if(n>=4)
                         t = atoi(mycub_cmd[3]) / 1000.0;
                     mycub.gotoPose(atoi(mycub_cmd[1]), atoi(mycub_cmd[2]), t);
-                    ack_serial(comfd, "[ok]");
+                    printf("[ok]"); fflush(stdout);
                 }
                 else
-                    ack_serial(comfd, "[error]");
+                    printf("[error]"); fflush(stdout);
             }
 
             else if(strcmp(mycub_cmd[0], "gotoPoseSync")==0 ) {
@@ -323,54 +189,49 @@ int myCub_main(int argc, char *argv[])
                     if(n>=4)
                         t = atoi(mycub_cmd[3]) / 1000.0;
                     mycub.gotoPoseSync(atoi(mycub_cmd[1]), atoi(mycub_cmd[2]), t);
-                    ack_serial(comfd, "[ok]");
+                    printf("[ok]"); fflush(stdout);
                 }
                 else
-                    ack_serial(comfd, "[error]");
+                    printf("[error]"); fflush(stdout);
             }            
-            else if(strcmp(mycub_cmd[0], "runDemo")==0 ) {
-                JointContext ctx = mycub.getContext();
-                mycub.gotoPose(FRONT_ARM, 160, 0.5);
-                mycub.gotoPose(BACK_ARM, 100, 0.5);
-                mycub.gotoPose(LEFT_ARM, 120, 0.5);
-                mycub.gotoPose(RIGHT_ARM, 120, 0.5);
-                while(!mycub.checkMotionDone()) sleep(1);
-                
-                should_stop = false;
-                while(!should_stop)
-                {
-                    int dist = mycub.getDistance(RIGHT_RANGE);
-                    if(dist == 0)
-                        printf("No obstacle in the range\n");
-                    else
-                        printf("Obstacle at distance %d\n", dist);
-
-                    if((dist == 0) || (dist > 150))
-                    {
-                        mycub.gotoPoseSync(BACK_ARM, 100, 0.2);
-                        mycub.gotoPoseSync(FRONT_ARM, 8, 1.0);
-                        
-                        mycub.gotoPose(LEFT_ARM, 70, 0.2);
-                        mycub.gotoPose(RIGHT_ARM, 70, 0.2);        
-                        while(!mycub.checkMotionDone()) sleep(1);
-
-                        mycub.gotoPoseSync(FRONT_ARM, 160, 1.0);
-                        
-                        mycub.gotoPoseSync(BACK_ARM, 80, 0.2);
-                        mycub.gotoPose(LEFT_ARM, 120, 0.2);
-                        mycub.gotoPose(RIGHT_ARM, 120, 0.2);        
-                        while(!mycub.checkMotionDone()) sleep(1);
-                    }
-                    else
-                        sleep(1);   //wait a bit
-                 }
+            else if(strcmp(mycub_cmd[0], "moveFront")==0 ) {
+               double t = 0.0;
+                if(n>=2)
+                    t = atoi(mycub_cmd[1]) / 1000.0;
+                MyCubWalker::moveFront(mycub, t);
+                printf("[ok]"); fflush(stdout);
             }
+            else if(strcmp(mycub_cmd[0], "moveBack")==0 ) {
+               double t = 0.0;
+                if(n>=2)
+                    t = atoi(mycub_cmd[1]) / 1000.0;
+                MyCubWalker::moveBack(mycub, t);
+                printf("[ok]"); fflush(stdout);
+            }
+            else if(strcmp(mycub_cmd[0], "moveRight")==0 ) {
+               double t = 0.0;
+                if(n>=2)
+                    t = atoi(mycub_cmd[1]) / 1000.0;
+                MyCubWalker::moveRight(mycub, t);
+                printf("[ok]"); fflush(stdout);
+            }
+            else if(strcmp(mycub_cmd[0], "moveLeft")==0 ) {
+               double t = 0.0;
+                if(n>=2)
+                    t = atoi(mycub_cmd[1]) / 1000.0;
+                MyCubWalker::moveLeft(mycub, t);
+                printf("[ok]"); fflush(stdout);
+            }
+            else if(strcmp(mycub_cmd[0], "stop")==0 ) {
+                MyCubWalker::stop();
+                printf("[ok]"); fflush(stdout);
+            }
+
         }
         //usleep(100000);
 	}
 
-	//tcsetattr(comfd,TCSANOW,&oldtio);
-	close(comfd);
+    MyCubWalker::fini();
     mycub.fini();
     printf("done!\n");
 
---
+++ firmware/apps/system/myCub/mycub_walk.cpp
@@ -0,0 +1,251 @@
+/****************************************************************************
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/* Collects and reports system information.
+ *
+ * TODO: Gather information also from low-level devices, kernel/sched, clock,
+ *   and further reporting as: sysinfo rtc, or sysinfo sched, ... with
+ *   sysinfo help to report all of the options.
+ */
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include <mycub_walk.h>
+
+extern "C"
+{
+
+typedef struct __wall_param_t {
+    MyCubInterface * mycub;
+    double t;
+    int front;
+    int back;
+    int right;
+    int left;
+    bool should_stop;
+    bool running;
+} walk_param_t;
+
+
+static walk_param_t walk_param; 
+static pthread_t walk_thread;
+pthread_mutex_t params_mutex;
+
+
+/* controller thread */
+ void * walker(void *pParams) 
+{
+    walk_param_t* params = (walk_param_t *) pParams;
+
+    pthread_mutex_lock(&(params_mutex));
+    double t = params->t;
+    int front = params->front;
+    int back = params->back;
+    int right = params->right;
+    int left = params->left;
+    bool should_stop = params->should_stop;    
+    params->running = true;
+    pthread_mutex_unlock(&(params_mutex));
+
+    if(should_stop)
+    {
+        pthread_mutex_lock(&(params_mutex));
+        params->running = false;
+        pthread_mutex_unlock(&(params_mutex));
+        return NULL;
+    }
+
+    params->mycub->gotoPose(front, 160, 0.1);
+    params->mycub->gotoPose(back, 100, 0.1);
+    params->mycub->gotoPose(left, 120, 0.1);
+    params->mycub->gotoPose(right, 120, 0.1);
+    while(!params->mycub->checkMotionDone()) usleep(300000);
+
+    while(!should_stop)
+    {
+        pthread_mutex_lock(&(params_mutex));
+        t = params->t;
+        front = params->front;
+        back = params->back;
+        right = params->right;
+        left = params->left;
+        params->running = true;
+        should_stop = params->should_stop;
+        pthread_mutex_unlock(&(params_mutex));
+
+        params->mycub->gotoPoseSync(back, 100, 0.2);
+        if(should_stop) break;
+        params->mycub->gotoPoseSync(front, 8, 1.0);        
+        if(should_stop) break;
+
+        params->mycub->gotoPose(left, 70, 0.2);
+        params->mycub->gotoPose(right, 70, 0.2);        
+        while(!params->mycub->checkMotionDone()) usleep(300000);
+        if(should_stop) break;
+
+        params->mycub->gotoPoseSync(front, 160, 1.0);
+        if(should_stop) break;
+
+        params->mycub->gotoPoseSync(back, 80, 0.2);
+        if(should_stop) break;
+        params->mycub->gotoPose(left, 120, 0.2);
+        params->mycub->gotoPose(right, 120, 0.2);        
+        while(!params->mycub->checkMotionDone()) usleep(300000);
+        if(should_stop) break;
+    }        
+
+    pthread_mutex_lock(&(params_mutex));
+    params->running = false;
+    pthread_mutex_unlock(&(params_mutex));
+    return NULL;
+}   
+
+} // end of extern "C"
+
+
+bool MyCubWalker::init(void)
+{
+    walk_param.running = false;
+    int status = pthread_mutex_init(&params_mutex, NULL);
+    if(status != 0) {
+        printf("MyCubWalker::init(): ERROR pthread_mutex_init failed, status=%d\n", status);
+        return false;
+    }        
+    return true;
+}
+
+void MyCubWalker::fini(void)
+{
+    MyCubWalker::stop();
+    pthread_mutex_destroy(&params_mutex);
+}
+
+void MyCubWalker::stop(void)
+{
+    pthread_mutex_lock(&(params_mutex));
+    walk_param.should_stop = true;
+    pthread_mutex_unlock(&(params_mutex));
+
+    pthread_join(walk_thread, NULL);
+    pthread_detach(walk_thread);
+    pthread_cancel(walk_thread);
+}
+
+bool MyCubWalker::moveFront(MyCubInterface& mycub, double t)
+{
+    MyCubWalker::stop();
+   
+    walk_param.mycub = &mycub;
+    walk_param.t = t;
+    walk_param.front = FRONT_JOINT;
+    walk_param.back = BACK_JOINT; 
+    walk_param.right = RIGHT_JOINT; 
+    walk_param.left = LEFT_JOINT; 
+    walk_param.should_stop = false;
+
+    int status = pthread_create(&walk_thread, NULL, walker, &walk_param);
+    if(status != 0)
+    {
+        printf("MyCubWalker::moveFront(): pthread_create failed, status=%d\n", status);
+        return false;
+    }
+    return true;
+}
+
+bool MyCubWalker::moveBack(MyCubInterface& mycub, double t)
+{
+    MyCubWalker::stop();
+   
+    walk_param.mycub = &mycub;
+    walk_param.t = t;
+    walk_param.front = BACK_JOINT;
+    walk_param.back = FRONT_JOINT; 
+    walk_param.right = RIGHT_JOINT; 
+    walk_param.left = LEFT_JOINT; 
+    walk_param.should_stop = false;
+
+    int status = pthread_create(&walk_thread, NULL, walker, &walk_param);
+    if(status != 0)
+    {
+        printf("MyCubWalker::moveBack(): pthread_create failed, status=%d\n", status);
+        return false;
+    }
+    return true;
+}
+
+bool MyCubWalker::moveRight(MyCubInterface& mycub, double t)
+{
+    MyCubWalker::stop();
+   
+    walk_param.mycub = &mycub;
+    walk_param.t = t;
+    walk_param.front = RIGHT_JOINT;
+    walk_param.back = LEFT_JOINT; 
+    walk_param.right = FRONT_JOINT; 
+    walk_param.left = BACK_JOINT; 
+    walk_param.should_stop = false;
+
+    int status = pthread_create(&walk_thread, NULL, walker, &walk_param);
+    if(status != 0)
+    {
+        printf("MyCubWalker::moveRight(): pthread_create failed, status=%d\n", status);
+        return false;
+    }
+    return true;
+}
+
+bool MyCubWalker::moveLeft(MyCubInterface& mycub, double t)
+{
+    MyCubWalker::stop();
+   
+    walk_param.mycub = &mycub;
+    walk_param.t = t;
+    walk_param.front = LEFT_JOINT;
+    walk_param.back = RIGHT_JOINT; 
+    walk_param.right = FRONT_JOINT; 
+    walk_param.left = BACK_JOINT; 
+    walk_param.should_stop = false;
+
+    int status = pthread_create(&walk_thread, NULL, walker, &walk_param);
+    if(status != 0)
+    {
+        printf("MyCubWalker::moveLeft(): pthread_create failed, status=%d\n", status);
+        return false;
+    }
+    return true;
+}
+
+
